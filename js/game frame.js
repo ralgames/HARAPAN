3d547ad0b9e0dc7b324e6b5593d89eb14bcac630
ralgames <smallkirby95@gmail.com>
2019-03-20 18:08:21 +0900

    シーンの型を作成

--- js/game frame.js
+++ js/game frame.js
@@ -1,3 +1,31 @@
+var gameScene = function() {
+  this.scene = 0; // 0-Title, 1-GameMain, 2-Result
+};
+
+gameScene.prototype = {
+  update: function() {
+    switch (this.scene) {
+      case 0: titleUpdate();  break;
+      case 1: mainUpdate();   break;
+      case 2: resultUpdate(); break;
+      default:
+        alert('無効な値です');
+    }
+  }
+};
+
+function titleUpdate() {
+  // Update
+}
+
+function mainUpdate() {
+  // Update
+}
+
+function resultUpdate() {
+  // Update
+}
+
 window.onload = function() {
   renderer();
 };

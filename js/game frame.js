57b116fd7ad05e9631535385459c23d6c1f13a40
ralgames <smallkirby95@gmail.com>
2019-03-20 17:43:04 +0900

    トップページにあるボタンのテキストを「調整中」に変更

--- js/game frame.js
+++ js/game frame.js
@@ -1,10 +1,8 @@
-window.onload = function ()
-{
+window.onload = function() {
   renderer();
 };
 
-function renderer()
-{
+function renderer() {
   var ctx = document.getElementById('canvas').getContext('2d');
   ctx.fillStyle = 'rgb(0, 0, 0)';
   ctx.fillRect(0, 0, 800, 600);
